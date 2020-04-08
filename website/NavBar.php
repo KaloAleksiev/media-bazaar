8eb72ec65314f6145bbf935ce8d3bef69157e680
Cristina <cristina.sona@yahoo.com>
2020-04-08 12:11:33 +0200

    The website first version

---
+++ website/NavBar.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+      <div class="topnav">
+        <div class="topnavtext">
+        <a>Log Out</a>
+          <a>Schedule</a>
+          <a>Stock</a>
+          <a>Profile</a>
+          <a class="active">Home</a>
+        
+        </div>
+      </div>
+
+    </div>
---
+++ website/index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="Styles.css">
+
+</head>
+
+<body>
+  <div class="background">
+  <img class="logo" src="logo.png"> 
+  <?php include('Navbar.php');?>
+  </div>
+
+ <!--?php include('Home.php');?-->
+</body>
+
+
+</html>
