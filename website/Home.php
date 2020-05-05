caf3c5031af093f80f03c2c08beeec6fd906914c
Cristina <cristina.sona@yahoo.com>
2020-05-05 23:21:49 +0200

    website: changed from localhost DB, fixed the issues with login

--- website/Home.php
+++ website/Home.php
@@ -1,6 +1,13 @@
 
 
-
+<?php  
+session_start();
+if(isset($_SESSION['username']))  
+ {  
+      echo '<h3>Login Success, Welcome - '.$_SESSION['username'].'</h3>';  
+ }  
+ 
+ ?> 
 <!DOCTYPE html>
 <html>
 
@@ -18,18 +25,32 @@
 
  
 <div class="nextShift">
-<?php  
-session_start();
-if(isset($_SESSION['username']))  
- {  
-      echo '<h3>Login Success, Welcome - '.$_SESSION['username'].'</h3>';  
- }  
- 
- ?> 
+
 <h1>Your next shift is going to be on :</h1>
 </div>
 <div class ="annoucements"> 
 <h1>Annoucemnts!</h1>
+<?php
+$conn = new mysqli("studmysql01.fhict.local", "dbi427262", "parola1234","dbi427262");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT title, text FROM announcement";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> Title: ". $row["title"]. " - Name: ". $row["text"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
 </div>
 </body>
 </html>
