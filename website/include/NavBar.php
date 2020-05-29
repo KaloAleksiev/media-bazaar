3e0a27ea22b02e99b9d493f52a3d967f42639cc2
Popov,Aleksandar A <a.popov@student.fontys.nl>
2020-05-29 15:03:41 +0200

    Changed 1 line literally

--- website/include/NavBar.php
+++ website/include/NavBar.php
@@ -6,7 +6,7 @@
           <a href="stock.php">Stock</a>
           <a href="profile.php">Profile</a>
           <a href="Home.php">Home</a>
-          <a href="preference.php">Home</a>
+          <a href="preference.php">Preferences</a>
         </div>
       </div>
 </div>
