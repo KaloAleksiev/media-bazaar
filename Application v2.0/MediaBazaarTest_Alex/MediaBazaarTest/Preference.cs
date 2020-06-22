a5e7638947838a9412fb7a4a179aa788e3fe6278
Vukašinović,Velimir V <v.vukashinovich@student.fontys.nl>
2020-06-22 03:05:33 +0200

    added preference chacking

---
+++ Application v2.0/MediaBazaarTest_Alex/MediaBazaarTest/Preference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarTest
+{
+    class Preference
+    {
+        public int UserId { get; private set; }
+        public string Type { get; private set; }
+        public string[] Days { get; private set; }
+
+        public Preference(int userId, string type, string[] days)
+        {
+            this.Type = type;
+            this.Days = days;
+            this.UserId = userId;
+        }
+    }
+}
---
+++ Application v2.0/MediaBazaarTest_Alex/MediaBazaarTest/PreferenceControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarTest
+{
+    class PreferenceControl
+    {
+        List<Preference> preferences;
+        PreferenceDataControl pdc;
+
+        public PreferenceControl()
+        {
+            preferences = new List<Preference>();
+            pdc = new PreferenceDataControl();
+            foreach (Preference p in pdc.GetAllPreferences())
+            { preferences.Add(p); }
+        }
+
+        public List<Preference> GetAllPreferences()
+        { return preferences; }
+
+        public Preference GetPreferenceByUserId(int userId)
+        {
+            foreach (Preference p in preferences)
+            {
+                if (p.UserId == userId)
+                { return p; }
+            }
+            return null;
+        }
+    }
+}
